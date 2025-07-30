cfba3c4cf66cf8c2eef929ef8e7fa94d25c7417a
Dharmik Parmar <dharmikparmar2004@yahoo.com>
2025-07-30 12:29:14 +0530

    made colorscheme transparent

---
+++ init.vim
@@ -0,0 +1,124 @@
+" Welcome message
+echo ">^.^<"
+
+" Basic settings
+set number
+set autoindent
+set mouse=i
+set tabstop=4
+set shiftwidth=4
+set smarttab
+set softtabstop=4
+set wrap
+set linebreak
+set nocompatible
+filetype plugin indent on
+syntax on
+
+" Vimwiki conceal syntax
+syntax match VimwikiConceal /^{{{.*\|^}}}.*/ conceal
+highlight link VimwikiConceal Conceal
+
+" Terminal mode escape mappings
+tnoremap <Esc> <C-\><C-n>
+tnoremap kj <C-\><C-n>
+
+" Insert mode quick escape
+inoremap jj <Esc>
+
+" Save file with Ctrl+S in all modes
+nnoremap <C-s> :w<CR>
+inoremap <C-s> <Esc>:w<CR>a
+vnoremap <C-s> <Esc>:w<CR>
+
+" NERDTree Mappings
+nnoremap <C-f> :NERDTreeFocus<CR>
+nnoremap <C-n> :NERDTree<CR>
+nnoremap <C-t> :NERDTreeToggle<CR>
+
+let g:NERDTreeWinSize = 20
+
+" coc.nvim settings
+let g:coc_disable_startup_warning = 1
+
+" Tab completion for coc.nvim
+inoremap <silent><expr> <Tab> pumvisible() ? coc#_select_confirm() : "\<C-g>u\<Tab>"
+inoremap <silent><expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<C-h>"
+
+" Run Python file with F9
+autocmd FileType python map <buffer> <F9> :w<CR>:exec '!python3' shellescape(@%, 1)<CR>
+autocmd FileType python imap <buffer> <F9> <esc>:w<CR>:exec '!python3' shellescape(@%, 1)<CR>
+
+" Resize windows with Ctrl + arrow keys
+nnoremap <C-Left> :vertical resize -2<CR>
+nnoremap <C-Right> :vertical resize +2<CR>
+nnoremap <C-Up> :resize +2<CR>
+nnoremap <C-Down> :resize -2<CR>
+
+" VimWiki Diary Mappings
+let g:vimwiki_diary_rel_path = 'diary'
+let g:vimwiki_auto_diary_index = 1
+
+nnoremap <Leader>dt :VimwikiMakeDiaryNote<CR>
+nnoremap <Leader>dy :VimwikiDiaryPrevDay<CR>
+nnoremap <Leader>dm :VimwikiDiaryNextDay<CR>
+nnoremap <Leader>di :VimwikiDiaryIndex<CR>
+nnoremap <Leader>gf :VimwikiRebuildIndex<CR>
+
+" VimWiki configuration
+"
+" HTML-generating Wiki: Uncomment this block to enable HTML generation
+let g:vimwiki_list = [{
+    \ 'path': '~/vimwiki/webwiki/',
+    \ 'syntax': 'default',
+    \ 'ext': '.wiki',
+    \ 'path_html': '~/from-my-github/dhrm1k.github.io/vimwiki_html/'
+    \ }]
+
+" Regular diary: Uncomment this block to enable the simple diary version
+" let g:vimwiki_list = [{
+"     \ 'path': '~/vimwiki/',
+"     \ 'syntax': 'default',
+"     \ 'ext': '.wiki',
+"     \ 'path_html': '~/from-my-github/dhrm1k.github.io/vimwiki_html/'
+"     \ }]
+
+" TOGGLE HELP:
+" Uncomment one block at a time.  
+" To remove comments quickly, in normal mode:
+" - Select the lines (Shift + V then move up/down)  
+" - Type: :s/^"\s\?// to uncomment lines  
+" - To comment, select and type: :s/^/"/  
+"
+
+" VimWiki HTML headers/footers
+let g:vimwiki_html_header = '~/vimwiki/header.html'
+let g:vimwiki_html_footer = '~/vimwiki/footer.html'
+
+
+" Plugins
+call plug#begin()
+Plug 'preservim/nerdtree'            " File tree
+Plug 'vim-airline/vim-airline'       " Status bar
+Plug 'tpope/vim-surround'            " Surrounding ysw
+Plug 'ryanoasis/vim-devicons'        " Developer Icons
+Plug 'ap/vim-css-color'              " CSS Color Preview
+Plug 'rafi/awesome-vim-colorschemes' " Retro Scheme
+" Plug 'neoclide/coc.nvim', {'branch': 'release'} " Auto Completion
+Plug 'vimwiki/vimwiki'               " VimWiki
+Plug 'sbdchd/neoformat'              " Code formatting
+Plug 'github/copilot.vim'            " GitHub Copilot for Vim
+call plug#end()
+
+" Colorscheme
+colorscheme gruvbox
+
+" Transparent background
+hi Normal guibg=NONE ctermbg=NONE
+hi NormalNC guibg=NONE ctermbg=NONE
+hi LineNr guibg=NONE ctermbg=NONE
+hi VertSplit guibg=NONE ctermbg=NONE
+hi EndOfBuffer guibg=NONE ctermbg=NONE
+hi StatusLine guibg=NONE ctermbg=NONE
+hi StatusLineNC guibg=NONE ctermbg=NONE
+
---
+++ temp_init.lua
@@ -0,0 +1,128 @@
+print(">^.^<")
+
+
+vim.opt.number = true
+
+
+vim.opt.autoindent = true
+
+vim.api.nvim_command('syntax match VimwikiConceal /^{{{.*\\|^}}}.*/ conceal')
+vim.api.nvim_set_hl(0, 'VimwikiConceal', { link = 'Conceal' })
+
+
+vim.opt.mouse = "i"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.softtabstop = 4
+
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+--Exit the terminal buffer.
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+
+vim.keymap.set('t', 'kj', '<C-\\><C-n>', { noremap = true, silent = true })
+
+-- VimWiki keybindings
+
+-- VimWiki settings
+vim.g.vimwiki_diary_rel_path = "diary"
+vim.g.vimwiki_auto_diary_index = 1
+
+-- Key mappings for VimWiki diary features
+vim.api.nvim_set_keymap("n", "<Leader>dt", ":VimwikiMakeDiaryNote<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>dy", ":VimwikiDiaryPrevDay<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>dm", ":VimwikiDiaryNextDay<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>di", ":VimwikiDiaryIndex<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<Leader>gf", ":VimwikiRebuildIndex<CR>", { noremap = true, silent = true })
+
+
+
+-- Map Ctrl-S to save the file in different modes
+vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>a", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
+
+
+--To exit terminal mode
+
+
+-- Plugins
+--
+vim.cmd [[
+  call plug#begin()
+
+  Plug 'preservim/nerdtree'        " File tree
+  Plug 'vim-airline/vim-airline'    " Status bar
+  Plug 'tpope/vim-surround'         " Surrounding ysw
+  Plug 'ryanoasis/vim-devicons'     " Developer Icons
+  Plug 'ap/vim-css-color'           " CSS Color Preview
+  Plug 'rafi/awesome-vim-colorschemes' " Retro Scheme
+  Plug 'neoclide/coc.nvim'          " Auto Completion
+  Plug 'vimwiki/vimwiki'           " Vimwiki
+
+  call plug#end()
+]]
+
+-- General Settings
+vim.opt.compatible = false
+vim.cmd('filetype plugin on')
+vim.cmd('syntax on')
+
+-- NERDTree Mappings
+vim.api.nvim_set_keymap("n", "<C-f>", ":NERDTreeFocus<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-n>", ":NERDTree<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-t>", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
+
+-- NERDTree Window Size
+vim.g.NERDTreeWinSize = 20
+
+-- YouCompleteMe settings
+vim.g.ycm_auto_trigger = 1
+vim.g.ycm_goto_previous_completion = '<C-p>'
+vim.g.ycm_goto_next_completion = '<C-n>'
+
+-- Coc.nvim settings
+vim.g.coc_disable_startup_warning = 1
+
+-- Colorscheme
+vim.cmd("colorscheme gruvbox")
+
+-- Tab Completion for Coc
+vim.api.nvim_set_keymap("i", "<Tab>", "pumvisible() ? coc#_select_confirm() : \"<C-g>u<Tab>\"", { noremap = true, silent = true, expr = true })
+
+--popups for language
+vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
+
+-- vim wiki html generation --
+vim.g.vimwiki_list = {
+  {
+	-- when want to generate html then below
+     path = "~/vimwiki/webwiki/",
+	-- regular diary below
+--	path = "~/vimwiki",
+
+
+	syntax = "default",
+    ext = ".wiki",
+    path_html = "~/from-my-github/dhrm1k.github.io/vimwiki_html/"
+  }
+}
+
+vim.g.vimwiki_html_header = "~/vimwiki/header.html"
+vim.g.vimwiki_html_footer = "~/vimwiki/footer.html"
+-- Keybinding to run Python code with F9
+vim.api.nvim_set_keymap('n', '<F9>', ':w !python3 %<CR>', { noremap = true, silent = true })
+
+
+-- Resize windows using Ctrl + Arrow keys
+vim.keymap.set('n', '<C-Left>',  ':vertical resize -2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-Up>',    ':resize +2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-Down>',  ':resize -2<CR>', { noremap = true, silent = true })
+
